5f1d05cba74d0dc89810c01ca3597dad8ce06b9f
May <aira.1996@yandex.ru>
2022-10-05 13:53:02 +0300

    -- убрать фичу

--- server/src/main/java/ru/practicum/explorewhithme/exception/CommentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.explorewhithme.exception;
-
-public class CommentNotFoundException extends RuntimeException {
-    public CommentNotFoundException(String message) {
-        super(message);
-    }
-}
